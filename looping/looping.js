ca026fe415995640c5576b6ee7ed759bb3661881
Rafiahmad567 <142226654+Rafiahmad567@users.noreply.github.com>
2023-11-22 12:45:24 +0700

    Add files via upload

---
+++ looping/looping.js
@@ -0,0 +1,15 @@
+for(let i = 1; i < 10; i ++){
+    console.log(`this is for ${i}`)
+}
+// looping while
+ let p = 1;
+ while (p <= 10 ){
+    console.log(`this is while ${p}`)
+    p++
+ }
+ // looping do while
+ let x = 1
+ do {
+    console.log(`this is do while ${x}`)
+    x++
+ } while (x <= 10);
